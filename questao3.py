f595c4727fb853d11dcb2e21493a12c4b23d6548
kleberson <kleberson.maria@ccc.ufcg.edu.br>
2023-04-12 23:00:02 -0300

    Adicionando questão 3.

--- questao3.py
+++ questao3.py
@@ -1,8 +1,31 @@
 import json
 
-file = open('dados.json')
+with  open('dados.json') as file:
+    dados = json.load(file)
 
-dados = jason.load(file)
+maximo = dados[0].get('valor')
+minimo = dados[0].get('valor')
+soma_valores = 0.0
+
+# contador dos dados válidos
+i = 0
+
 
 for dado in dados:
-    if(dado.
+    valor = dado.get('valor')
+    if(valor > maximo):
+        maximo = valor
+    if (valor < minimo):
+        minimo = valor
+    if(valor != 0 ):
+        i+=1
+        soma_valores += valor
+
+media = soma_valores/i
+
+print('Maior valor: {}'.format(maximo))
+print('Menor valor: {}'.format(minimo))
+print('Media: {}'.format(media))
+
+
+
